7cbaf0de4ffad47a90c034499fe1a2d5aa97b758
nisac <nisacolak@outlook.com>
2021-05-11 21:14:10 +0300

    11.05.2021

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import {DatabaseService} from '../services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todos=[];
+  constructor(private databaseProvider:DatabaseService,private toast:ToastController) {}
+  ngOnInit(): void {}
 
-  constructor() {}
+  ionViewWillEnter(){
+    this.loadTodos();
+  }
 
-}
+  async presentToast() {
+      const toast = await this.toast.create({
+        message: 'kayıtlar silindi',
+        duration: 2000
+      });
+      toast.present();
+    }
+    loadTodos(){
+      this.databaseProvider.getAllTodo().then(todos=>{
+        this.todos=todos;
+      });
+    }
+
+    deleteTodo(id){
+      this.databaseProvider.deleteTodo(id);
+      this.loadTodos();
+      this.presentToast();
+    }
+  }
+
+
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import {SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
+import { data } from 'jquery';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  database:SQLiteObject;
+  constructor(private sqlite:SQLite) {
+    this.sqlite.create({
+      name:'todo.db',
+      location:'default'
+    })
+    .then((db:SQLiteObject)=>{
+      db.executeSql('create table IF NOT EXISTS todo(ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, desc TEXT)',[]);
+      this.database=db;
+    })
+    .catch(e=>console.log(e));
+   }
+   addTodo(title,desc){
+    return this.database.executeSql("insert into todo (title,desc) values ('"+title+"','"+desc+"')").then((data)=>{
+      alert('veri eklendi');
+      return data;
+    });
+   }
 
-  constructor() { }
+   getAllTodo(){
+    return this.database.executeSql('select * from todo',[]).then((data)=>{
+      let todos=[];
+      if(data.rows.length>0){
+        for(let i=0;i<data.rows.length;i++){
+          todos.push({ID:data.rows.item(i).ID,title:data.rows.item(i).title, desc:data.rows.item(i).desc});
+        }
+      }
+        return todos;
+    });
+   }
+
+   updateTodo(id){
+    return this.database.executeSql('select * from todo where ID='+id,[]).then((data)=>{
+      let todos = [];
+      if(data.rows.length>0){
+        for(let i=0;i<data.rows.length;i++){
+          todos.push({ID:data.rows.item(i).ID, title:data.rows.item(i).title,desc:data.rows.item(i).desc});    
+        }
+      }
+      return todos;
+    });
+   }
+
+   updateTodoSave(title,desc,id){
+    return this.database.executeSql("update todo set title='"+title+"',desc='"+desc+"'where ID="+id).then((data)=>{
+      alert('güncellenme başarılı');
+      return data;
+    });
+   }
+
+   deleteTodo(id){
+     this.database.executeSql('delete from todo where ID='+id);
+   }
 }
